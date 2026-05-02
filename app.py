93adb187b3ec0375171a144f15bf3316100f8bbe
LuziferFakeTH <boripath.h.s@gmail.com>
2026-05-02 13:43:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ app = Flask(__name__)
 # ---------------------------
 def get_db():
     DATABASE_URL = os.environ.get("DATABASE_URL")
+
+    if not DATABASE_URL:
+        raise Exception("❌ DATABASE_URL not set in environment")
+
     return psycopg2.connect(DATABASE_URL)
 
 # ---------------------------
