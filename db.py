3f7733c0a91a3b810a50abac3bbc3e2cab98a189
LuziferFakeTH <boripath.h.s@gmail.com>
2026-05-06 12:59:26 +0700

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,10 @@
+import psycopg2
+import os
 
+def get_db():
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+
+    if not DATABASE_URL:
+        raise Exception("❌ DATABASE_URL not set")
+
+    return psycopg2.connect(DATABASE_URL)
