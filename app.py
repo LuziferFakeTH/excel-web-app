397a7e0d0ba981ea32f164c8dace0c516d804557
LuziferFakeTH <boripath.h.s@gmail.com>
2026-05-06 10:20:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,26 @@ import pytz
 
 app = Flask(__name__)
 
+def get_storage_info():
+    conn = get_db()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT pg_database_size(current_database());")
+    used_bytes = cursor.fetchone()[0]
+
+    conn.close()
+
+    # PostgreSQL free plan สมมติ 1GB
+    total_bytes = 1024 * 1024 * 1024
+
+    remaining_bytes = total_bytes - used_bytes
+    percent_used = (used_bytes / total_bytes) * 100
+
+    return {
+        "used_kb": round(used_bytes / 1024, 2),
+        "remaining_kb": round(remaining_bytes / 1024, 2),
+        "percent_used": round(percent_used, 2)
+    }
 # ---------------------------
 # CONNECT DATABASE (PostgreSQL)
 # ---------------------------
@@ -63,7 +83,8 @@ init_db()
 # ---------------------------
 @app.route("/")
 def index():
-    return render_template("index.html", results=None)
+    storage = get_storage_info()
+    return render_template("index.html", storage=storage)
 
 # ---------------------------
 # 
